2053f7cdff7bb87f753c48cf7bd351437d4c0e13
Taha <tahaberserk@gmail.com>
2020-01-29 16:41:54 +0100

    commit map based service

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/service/map/OwnerServiceMap.java
@@ -0,0 +1,39 @@
+package com.springframework.sfgpetclinic.service.map;
+
+import java.util.Set;
+
+import com.springframework.sfgpetclinic.model.Owner;
+import com.springframework.sfgpetclinic.service.CrudService;
+
+public abstract class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+	@Override
+	public Set<Owner> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	
+}
+
+
+
